8c1bd511cb0d0741ce53de46068f8e3e625ea85d
bat dombry <baptiste.dombry12@gmail.com>
2024-02-21 12:40:03 +0100

    Ajout composant list-pokemon + pokemon

---
+++ src/app/component/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit} from '@angular/core';
+import { Pokemon } from '../../Entity/Pokemon';
+import { PokemonService } from '../../Services/pokemon.service';
+import { CommonModule } from '@angular/common';
+import { PokemonComponent } from '../pokemon/pokemon.component';
+
+@Component({
+  selector: 'app-list-pokemon',
+  standalone: true,
+  imports: [CommonModule, PokemonComponent],
+  templateUrl: "list-pokemon.component.html",
+  styleUrl: "list-pokemon.component.css"
+})
+export class ListPokemonComponent implements OnInit{
+  @Input() generationId: number;
+
+  pokemons: Pokemon[];
+  pokemonsWithoutMale: Pokemon[];
+
+  constructor(public pokemonService: PokemonService) { }
+
+  ngOnInit(): void
+  {
+    this.pokemons = this.pokemonService.getPokemonByGeneration(this.generationId);
+    this.pokemonsWithoutMale = this.pokemons.filter(p => p.femaleSprite != null); 
+  }
+
+}
