3ed11d930b08474fde7557be9af09b7d876227d7
bat dombry <baptiste.dombry12@gmail.com>
2024-02-21 12:39:21 +0100

    Modification app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
-  title = 'ng-poke-tracker';
-}
+
+export class AppComponent{ }
