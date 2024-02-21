aa516d67ab9cbd0966dcd5e93c240e4f41f6ec82
bat dombry <baptiste.dombry12@gmail.com>
2024-02-21 12:39:44 +0100

    Ajout composant navbar

---
+++ src/app/component/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatIconModule } from '@angular/material/icon'
+import { ListPokemonComponent } from '../list-pokemon/list-pokemon.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatTabsModule, MatIconModule, ListPokemonComponent],
+  templateUrl: "navbar.component.html",
+  styleUrl: "navbar.component.css",
+})
+export class NavbarComponent {
+
+}
