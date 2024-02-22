c2e00eba6d41420d17fe98d76aa4d8d2fe8c5d21
bat dombry <baptiste.dombry12@gmail.com>
2024-02-22 15:29:58 +0100

    Format code

--- src/app/Services/localStorage.service.ts
+++ src/app/Services/localStorage.service.ts
@@ -2,44 +2,38 @@ import { DOCUMENT } from "@angular/common";
 import { Inject, Injectable } from "@angular/core";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
-
 export class LocalStorageService {
+  localStorage: Storage | undefined;
 
-    localStorage: Storage | undefined;
+  constructor(@Inject(DOCUMENT) document: Document) {
+    this.localStorage = document.defaultView?.localStorage;
+  }
 
-    constructor(@Inject(DOCUMENT) document: Document) {
-        this.localStorage = document.defaultView?.localStorage;
+  public saveData(key: string, value: string) {
+    if (this.localStorage) {
+      this.localStorage.setItem(key, value);
     }
+  }
 
-    public saveData(key: string, value: string) {
-        if(this.localStorage)
-        {
-            this.localStorage.setItem(key, value);
-        }
+  public getData(key: string) {
+    if (this.localStorage) {
+      return this.localStorage.getItem(key);
     }
+    return null;
+  }
 
-    public getData(key: string) {
-        if(this.localStorage)
-        {
-            return this.localStorage.getItem(key);
-        }
-        return null;
+  public removeData(key: string) {
+    if (this.localStorage) {
+      return localStorage.removeItem(key);
     }
+    return null;
+  }
 
-    public removeData(key: string) {
-        if(this.localStorage)
-        {
-            return localStorage.removeItem(key);
-        }
-        return null;
+  public clearData() {
+    if (this.localStorage) {
+      localStorage.clear();
     }
-
-    public clearData() {
-        if(this.localStorage)
-        {
-            localStorage.clear();
-        }
-    }
+  }
 }
