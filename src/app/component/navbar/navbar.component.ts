608076d68c4f6710840c3b5a735f4195d1947ca9
bat dombry <baptiste.dombry12@gmail.com>
2024-02-28 18:54:36 +0100

    Ajout bouton menu

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from '@angular/material/button';
 import { ListPokemonComponent } from "../list-pokemon/list-pokemon.component";
 
 @Component({
   selector: "app-navbar",
   standalone: true,
-  imports: [MatTabsModule, MatIconModule, ListPokemonComponent],
+  imports: [MatTabsModule, MatIconModule, MatMenuModule, MatButtonModule, ListPokemonComponent],
   templateUrl: "navbar.component.html",
   styleUrl: "navbar.component.css",
 })
